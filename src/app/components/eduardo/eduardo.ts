8ecff50da4dc3782d053618d3912ca6b8d456bb6
Eduardo-L5 <23040084@alumno.utc.edu.mx>
2025-10-29 19:28:06 -0600

    Agregacion de componentes

---
+++ src/app/components/eduardo/eduardo.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-eduardo',
+  imports: [],
+  templateUrl: './eduardo.html',
+  styleUrl: './eduardo.css'
+})
+export class Eduardo {
+
+}
